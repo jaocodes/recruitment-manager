0e1ac69696eba71318e1c8eac974d6c9a1d3cb69
jaocodes <jhonskaz@gmail.com>
2025-11-18 17:13:39 -0300

    creating CandidateRepository extending using JPARepository

---
+++ src/main/java/br/com/jotace/recruitment_manager/modules/candidate/CandidateRepository.java
@@ -0,0 +1,8 @@
+package br.com.jotace.recruitment_manager.modules.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+}
