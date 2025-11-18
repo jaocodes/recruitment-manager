d2a844c6ebefa8a1d981bfb7700da61b1f48d154
jaocodes <jhonskaz@gmail.com>
2025-11-18 10:01:51 -0300

    creating first post route to create a candidate

---
+++ src/main/java/br/com/jotace/recruitment_manager/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,19 @@
+package br.com.jotace.recruitment_manager.modules.candidate.controllers;
+
+import br.com.jotace.recruitment_manager.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidate) {
+        System.out.println("Candidato");
+        System.out.println(candidate.getEmail());
+    }
+
+}
