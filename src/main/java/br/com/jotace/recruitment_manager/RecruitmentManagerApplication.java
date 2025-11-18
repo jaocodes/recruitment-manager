d9d12b94cb9736c47260fe88956871d33088ec34
jaocodes <jhonskaz@gmail.com>
2025-11-18 09:18:54 -0300

    setup project

---
+++ src/main/java/br/com/jotace/recruitment_manager/RecruitmentManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.jotace.recruitment_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentManagerApplication.class, args);
+	}
+
+}
