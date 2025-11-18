27784fa8dd37199ed67239f666f5835399803ffe
jaocodes <jhonskaz@gmail.com>
2025-11-18 15:57:22 -0300

    implementing a global exception handler using ControllerAdvice annotation

---
+++ src/main/java/br/com/jotace/recruitment_manager/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,33 @@
+package br.com.jotace.recruitment_manager.exceptions;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+    private MessageSource messageSource;
+
+    public ExceptionHandlerController (MessageSource message){
+        this.messageSource = message;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        List<ErrorMessageDTO> errDTO = new ArrayList<>();
+
+        e.getBindingResult().getFieldErrors().forEach(err -> {
+            String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
+            errDTO.add(new ErrorMessageDTO(message, err.getField()));
+        });
+
+        return new ResponseEntity<>(errDTO, HttpStatus.BAD_REQUEST);
+    }
+}
