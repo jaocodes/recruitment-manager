f1225e9c977cdd956aa95ebc0e7f085d6d832154
jaocodes <jhonskaz@gmail.com>
2025-11-18 10:11:16 -0300

    add request body validation

--- src/main/java/br/com/jotace/recruitment_manager/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/jotace/recruitment_manager/modules/candidate/CandidateEntity.java
@@ -1,14 +1,23 @@
 package br.com.jotace.recruitment_manager.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
 import java.util.UUID;
 
 @Data
 public class CandidateEntity {
     private UUID id;
     private String name;
+    @Pattern(regexp = "^(?!\\s*$).+" , message = "O campo [username] não deve conter espaços")
     private  String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 10, max=100)
     private String password;
     private String description;
     private String curriculum;
