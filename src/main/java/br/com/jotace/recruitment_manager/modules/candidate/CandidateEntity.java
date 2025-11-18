e6cebf5e5604f4c596b072b6fb682637484f2c07
jaocodes <jhonskaz@gmail.com>
2025-11-18 16:42:57 -0300

    implement entity mapping for Candidate

--- src/main/java/br/com/jotace/recruitment_manager/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/jotace/recruitment_manager/modules/candidate/CandidateEntity.java
@@ -1,14 +1,23 @@
 package br.com.jotace.recruitment_manager.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     @Pattern(regexp = "\\S+" , message = "O campo [username] não deve conter espaços")
@@ -21,4 +30,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
