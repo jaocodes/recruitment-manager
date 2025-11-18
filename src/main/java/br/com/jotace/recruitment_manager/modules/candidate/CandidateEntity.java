180dde15e74deaa9b48b6e397ab60286ef4ab42a
jaocodes <jhonskaz@gmail.com>
2025-11-18 14:22:16 -0300

    fix regexp to validade username field

--- src/main/java/br/com/jotace/recruitment_manager/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/jotace/recruitment_manager/modules/candidate/CandidateEntity.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class CandidateEntity {
     private UUID id;
     private String name;
-    @Pattern(regexp = "^(?!\\s*$).+" , message = "O campo [username] não deve conter espaços")
+    @Pattern(regexp = "\\S+" , message = "O campo [username] não deve conter espaços")
     private  String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
