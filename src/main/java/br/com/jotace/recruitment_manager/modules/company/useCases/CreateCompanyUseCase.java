260e19b608069000c08cdbc5b61518fed41c80cb
jaocodes <jhonskaz@gmail.com>
2025-11-19 15:45:23 -0300

    creating service layer to create a company entity

---
+++ src/main/java/br/com/jotace/recruitment_manager/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,22 @@
+package br.com.jotace.recruitment_manager.modules.company.useCases;
+
+import br.com.jotace.recruitment_manager.exceptions.UserFoundException;
+import br.com.jotace.recruitment_manager.modules.company.entities.CompanyEntity;
+import br.com.jotace.recruitment_manager.modules.company.entities.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCompanyUseCase {
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public CompanyEntity execute(CompanyEntity company){
+        this.companyRepository.findByUsernameOrEmail(company.getUsername(), company.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.companyRepository.save(company);
+    }
+}
