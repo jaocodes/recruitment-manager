a0a26155498c6c99d8ceb33ba1b31f0aeeecafd7
jaocodes <jhonskaz@gmail.com>
2025-11-19 16:26:18 -0300

    add controller to create a company

---
+++ src/main/java/br/com/jotace/recruitment_manager/modules/company/CompanyController.java
@@ -0,0 +1,29 @@
+package br.com.jotace.recruitment_manager.modules.company;
+
+import br.com.jotace.recruitment_manager.modules.company.entities.CompanyEntity;
+import br.com.jotace.recruitment_manager.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+    @Autowired
+    private CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity company){
+        try{
+            var result = this.createCompanyUseCase.execute(company);
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+}
