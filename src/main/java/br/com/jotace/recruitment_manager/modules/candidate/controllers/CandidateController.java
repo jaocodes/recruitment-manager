2ba45821ab7956b35fe6c3a7332500de85740773
jaocodes <jhonskaz@gmail.com>
2025-11-18 22:36:00 -0300

    refactor endpoint return to handle the error response properly

--- src/main/java/br/com/jotace/recruitment_manager/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/jotace/recruitment_manager/modules/candidate/controllers/CandidateController.java
@@ -3,8 +3,11 @@ package br.com.jotace.recruitment_manager.modules.candidate.controllers;
 import br.com.jotace.recruitment_manager.exceptions.UserFoundException;
 import br.com.jotace.recruitment_manager.modules.candidate.CandidateEntity;
 import br.com.jotace.recruitment_manager.modules.candidate.CandidateRepository;
+import br.com.jotace.recruitment_manager.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
-       this.candidateRepository
-               .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
-               .ifPresent((user) -> {
-                   throw new UserFoundException();
-               });
-       return this.candidateRepository.save(candidate);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidate) {
+
+        try{
+            var result =  this.createCandidateUseCase.execute(candidate);
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
 }
