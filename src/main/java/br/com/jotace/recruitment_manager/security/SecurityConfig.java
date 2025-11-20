825a92e6e83367b798e9ac127668bb7a18c17599
jaocodes <jhonskaz@gmail.com>
2025-11-20 16:12:55 -0300

    add config for public and private routes

--- src/main/java/br/com/jotace/recruitment_manager/security/SecurityConfig.java
+++ src/main/java/br/com/jotace/recruitment_manager/security/SecurityConfig.java
@@ -8,12 +8,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf-> csrf.disable())
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/candidate/").permitAll()
+                        .requestMatchers("/company/").permitAll()
+                        .anyRequest().authenticated()
 
-        ;
-
-        return http.build();
+                )
+                .build();
     }
-
 }
