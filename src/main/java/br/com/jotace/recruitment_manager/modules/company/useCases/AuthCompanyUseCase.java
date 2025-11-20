22a7273a02f1551bab824d54543f7d0156a238f6
jaocodes <jhonskaz@gmail.com>
2025-11-20 19:14:15 -0300

    add expiresAt in jwt

--- src/main/java/br/com/jotace/recruitment_manager/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/jotace/recruitment_manager/modules/company/useCases/AuthCompanyUseCase.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.naming.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
 
 
 @Service
@@ -38,6 +40,7 @@ public class AuthCompanyUseCase {
 
         var token = JWT.create()
                 .withIssuer("javagas")
+                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(company.getId().toString())
                 .sign(algorithm);
 
