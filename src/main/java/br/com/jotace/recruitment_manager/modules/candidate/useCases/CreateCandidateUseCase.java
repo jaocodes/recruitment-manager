32d86c32ed8482c09bae57fe71c59aee70890323
jaocodes <jhonskaz@gmail.com>
2025-11-21 17:30:07 -0300

    add password encoder on create candidate

--- src/main/java/br/com/jotace/recruitment_manager/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/jotace/recruitment_manager/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.jotace.recruitment_manager.exceptions.UserFoundException;
 import br.com.jotace.recruitment_manager.modules.candidate.CandidateEntity;
 import br.com.jotace.recruitment_manager.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,19 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidate){
         this.candidateRepository
                 .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(password);
+
         return this.candidateRepository.save(candidate);
     }
 }
